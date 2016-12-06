584290ffc9fefbcd705f9bac2277f99bd4ef54fc
Ashley Burton <s168972@ucs.ac.uk>
2016-12-06 23:07:33 +0000

    Further developed sight

--- Unity Project/GameAI/Assets/Scripts/AI.js
+++ Unity Project/GameAI/Assets/Scripts/AI.js
@@ -2,6 +2,7 @@
 
 public class viewcones 
 {
+	@header ("Set the distance of the viewcone, angle of viewcone and alert status of it")
 	public var distance: float;
 	public var angle: float;
 	public var status: statuses;
@@ -18,9 +19,14 @@ public var viewAngle: float;
 public var hit: RaycastHit;
 public var playerSeen: boolean;
 public var viewconeSeen: int;
-public var leftRay: Vector3;
-public var rightRay: Vector3;
-
+public var suspiciousTimer: float;
+public var suspiciousCheck: boolean;
+public var suspiciousTimerEnd: float;
+public var loseSightTimer: float;
+public var loseSightCheck: boolean;
+public var loseSightTimerEnd: float;
+public var currentTime: float;
+public var point: Animator;
 
 function Start () 
 {
@@ -33,11 +39,13 @@ function Start ()
 
 function Update () 
 {
-	
+	//Temp. Just to see current scene time in inspector
+	currentTime = Time.time;
 }
 
 function OnTriggerStay(other: Collider) 
 {
+
 	//Checks if player is inside of the trigger
 	if(other.gameObject.tag == "Player")
 	{
@@ -48,7 +56,6 @@ function OnTriggerStay(other: Collider)
 	
 		//Sets playerSeen to false. If it doesnt get set back to true then the player wasn't seen in the last checks.
 		playerSeen = false;
-		
 		//Starts a for loop running through each viewcone
 		for(var i: int = 0; i < viewconeInfo.Length; i++)
 		{	
@@ -61,13 +68,13 @@ function OnTriggerStay(other: Collider)
 					//Draws a debug ray inside of the scene view. Only use it to help see where the raycast is going
 					Debug.DrawRay(transform.position, playerDistance.normalized, Color.red);
 				
-					//Checks to see if the raycast hits the player. If it doesnt then the player is behind an object
+					//Checks to see if the raycast hits the player. If it doesn't then the player is behind an object
 					if(hit.collider.gameObject.tag == "Player")
 					{
 						//If the player hasn't already been seen then they have been now
 						if(!playerSeen)
 						{
-							alertStatus = viewconeInfo[i].status;
+							SetAlert(viewconeInfo[i].status);
 							Debug.Log(viewconeInfo[i].angle);
 							
 							playerSeen = true;
@@ -76,15 +83,81 @@ function OnTriggerStay(other: Collider)
 						//If player has been seen but viewcone is closer to the player then change spotted viewcone
 						else if (playerSeen && viewconeSeen > i)
 						{
-							alertStatus = viewconeInfo[i].status;
+							SetAlert(viewconeInfo[i].status);
 							Debug.Log(viewconeInfo[i].angle);
 							
 							playerSeen = true;
 							viewconeSeen = i;
 						}
+
 					}
 				}
 			}
+
+			//if it is the last viewcone being checked and player wasn't seen then set patrolling
+			if(i == viewconeInfo.Length - 1 && !playerSeen)
+			{
+				SetAlert(statuses.patrolling);
+			}
 		}
+	}
+}
+
+function SetAlert(status: statuses)
+{
+
+//Wait for half a second before any decision is made
+	yield WaitForSeconds(0.5);
+
+	//Check inputed function
+	switch(status)
+	{
+		//If status was alert set the AI alert status to alert and make sure that loseSight isn't true
+		case statuses.alert:	alertStatus = status;
+								loseSightCheck = false;
+								point.SetBool("Alert", true);
+								point.SetBool("Suspicious", false);
+								break;
+		//If suspicious then check if already alert. If not check how long they have been in the viewcone for. If more than the check timer then become alert 
+		case statuses.suspicious:	if(alertStatus == statuses.alert)
+									{
+										alertStatus = statuses.alert;
+									}
+									else
+									{
+										alertStatus = status;
+										point.SetBool("Alert", false);
+										point.SetBool("Suspicious", true);
+									}
+
+									if(!suspiciousCheck)
+									{
+										suspiciousCheck = true;
+										suspiciousTimerEnd = suspiciousTimer + Time.time;
+									}
+									else if(suspiciousCheck && Time.time > suspiciousTimerEnd)
+									{
+										alertStatus = statuses.alert;
+									}
+									loseSightCheck = false;
+									break;
+		//If player is no longer in a viewcone check how long they have not been seen for. If more than lose sight time then set back to partrolling
+		case statuses.patrolling:	if(!loseSightCheck)
+									{
+										loseSightCheck = true;
+										loseSightTimerEnd = loseSightTimer + Time.time;
+									}
+									else if(alertStatus == statuses.alert && loseSightCheck && Time.time < loseSightTimerEnd)
+									{
+										alertStatus = statuses.alert;
+									}
+									else
+									{
+										alertStatus = status;
+										point.SetBool("Alert", false);
+										point.SetBool("Suspicious", false);
+									}
+									suspiciousCheck = false;
+									break;
 	}
 }
