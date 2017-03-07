6fd1d54e564b173af3940d68b29f565ae109672e
Ashley Burton <s168972@ucs.ac.uk>
2017-03-07 18:09:26 +0000

    More A star work

--- Unity Project/GameAI/Assets/Scripts/AStar.cs
+++ Unity Project/GameAI/Assets/Scripts/AStar.cs
@@ -9,20 +9,56 @@ public class AStar : MonoBehaviour {
     public float totalNodesX;
     public float totalNodesY;
     public Vector2 pathSize;
-    Dictionary<Vector3, bool> nodes = new Dictionary<Vector3, bool>();
+	Node[,] nodes;
 	public GameObject player;
 	public Vector3 playerPosRounded;
 	public Vector3 other;
+	List<Node> path = new List<Node>();
+
+	public class Node
+	{
+		public bool walkable;
+		public Vector3 position;
+
+		public int hCost;
+		public int gCost;
+
+		public int xposition;
+		public int yposition;
+
+		public Node previousNode;
+
+		public int fCost()
+		{
+			return gCost + hCost;
+		}
+
+		public Node(bool walk, Vector3 pos, int xPos, int yPos)
+		{
+			walkable = walk;
+			position = pos;
+			xposition = xPos;
+			yposition = yPos;
+
+		}
+	}
 
     public void Start()
     {
+		//Calculates total amount of nodes needed
         totalNodesX = pathSize.x / nodeSize;
         totalNodesY = pathSize.y / nodeSize;
 
-        Vector3 botLeft;
+		//Finds where the grid will start to generate
+		Vector3 botLeft;
         botLeft.x = transform.position.x - pathSize.x / 2;
         botLeft.y = transform.position.x - pathSize.y / 2;
 
+		//Sets the amount of nodes for the array
+		nodes = new Node[int.Parse(totalNodesX.ToString()),int.Parse(totalNodesY.ToString())];
+
+
+		//Generates all the nodes checking if the node is blocked by any objects
         for (int i = 0; i < totalNodesX; i++)
         {
             for (int j = 0; j < totalNodesY; j++)
@@ -38,30 +74,75 @@ public class AStar : MonoBehaviour {
                     canWalk = false;
                 }
 
-                nodes[position] = canWalk;
+				nodes[i,j] = new Node(canWalk, position, i, j);
             }
         }
     }
 
-	public Vector3 GetNodePercent(Vector3 playerPos)
+	public void Update()
 	{
-		float percentX = (playerPos.x + pathSize.x / 2) / pathSize.x;
-		float percentY = (playerPos.z + pathSize.y / 2) / pathSize.y;
-
-		percentX = Mathf.RoundToInt(percentX);
-		percentY = Mathf.RoundToInt(percentY);
-
-		return new Vector3(percentX, playerPos.y, percentY);
+		//Gets path needed
+		GetPath();
 	}
 
-	public KeyValuePair<Vector3, bool> GetClosestNode(Vector3 playerPos)
+	void OnDrawGizmos()
 	{
-		KeyValuePair<Vector3, bool> nearest = new KeyValuePair<Vector3, bool>();
+		//If the game is playing draw gizmos showing the path and where can/can't be walked
+		if(Application.isPlaying)
+		{
+			Gizmos.DrawWireCube(transform.position, new Vector3(pathSize.x, 1, pathSize.y));
+			Node nearestNode = GetClosestNode(player.transform.position);
+
+			if (nodes != null)
+			{
+				foreach (Node node in nodes)
+				{
+					Vector3 key = node.position;
+					key.x = Mathf.RoundToInt(node.position.x);
+					key.z = Mathf.RoundToInt(node.position.z);
+					Vector3 pos = player.transform.position;
+					pos.x = Mathf.RoundToInt(pos.x);
+					pos.y = 0;
+					pos.z = Mathf.RoundToInt(pos.z);
+
+					if(node.position == nearestNode.position)
+					{
+						Gizmos.color = Color.blue;
+						other = key;
+						playerPosRounded = pos;
+					}
+					else if(node.walkable)
+					{
+						Gizmos.color = Color.green;
+					}
+					else
+					{
+						Gizmos.color = Color.red;
+					}
+					if(path.Contains(node))
+					{
+						Gizmos.color = Color.grey;
+					}
+						
+					if(node.position == nearestNode.position)
+					{
+						Gizmos.color = Color.blue;
+					}
+					Gizmos.DrawCube(node.position, Vector3.one * (nodeSize - 0.1f));
+				}
+			}
+		}
+	}
+
+	public Node GetClosestNode(Vector3 playerPos)
+	{
+		Node nearest = new Node(false, new Vector3(100000,100000,100000), 0 ,0);
 		double nearestDiff = 10000000000;
 
+		//Checks all the nodes finding the closes node to the inputed vector3
 		foreach (var node in nodes) 
 		{
-			float diff = Vector3.Distance(node.Key, playerPos);
+			float diff = Vector3.Distance(node.position, playerPos);
 			if(diff < nearestDiff)
 			{
 				nearest = node;
@@ -69,44 +150,128 @@ public class AStar : MonoBehaviour {
 			}
 		}
 
-		return nearest;
+			return nearest;
 	}
 
+	public void GetPath()
+	{
+		//Finds the start and end node
+		Node startNode = GetClosestNode(transform.position);
+		Node endNode = GetClosestNode(player.transform.position);
 
-    void OnDrawGizmos()
-    {
-        Gizmos.DrawWireCube(transform.position, new Vector3(pathSize.x, 1, pathSize.y));
-		KeyValuePair<Vector3, bool> nearestNode = GetClosestNode(player.transform.position);
+		List<Node> openNodes = new List<Node>();
+		List<Node> closedNodes = new List<Node>();
 
-        if (nodes != null)
-        {
-            foreach (KeyValuePair<Vector3, bool> node in nodes)
-            {
-				Vector3 key = node.Key;
-				key.x = Mathf.RoundToInt(node.Key.x);
-				key.z = Mathf.RoundToInt(node.Key.z);
-				Vector3 pos = player.transform.position;
-				pos.x = Mathf.RoundToInt(pos.x);
-				pos.y = 0;
-				pos.z = Mathf.RoundToInt(pos.z);
+		openNodes.Add(startNode);
 
-				if(node.Key == nearestNode.Key)
+		//While there are nodes that need checking go through them all finding the shortest path possible
+		while (openNodes.Count > 0) 
+		{
+			Node currentNode = openNodes[0];
+
+			for (int i = 1; i < openNodes.Count; i++) 
+			{
+				if(openNodes[i].fCost() < currentNode.fCost() || openNodes[i].fCost() == currentNode.fCost() && openNodes[i].hCost < currentNode.hCost)
 				{
-					Gizmos.color = Color.blue;
-					other = key;
-					playerPosRounded = pos;
+					currentNode = openNodes[i];
 				}
-				else if(node.Value)
-				{
-					Gizmos.color = Color.green;
-				}
-				else
-                {
-                    Gizmos.color = Color.red;
-                }
+			}
 
-                Gizmos.DrawCube(node.Key, Vector3.one * (nodeSize - 0.1f));
-            }
-        }
-    }
+			openNodes.Remove(currentNode);
+			closedNodes.Add(currentNode);
+
+			if(currentNode == endNode)
+			{
+			
+				//Removes any path that was there before and add all the nodes for the next path
+				path.Clear();
+				
+				while(currentNode != startNode)
+				{
+					path.Add(currentNode);
+					currentNode = currentNode.previousNode;
+				}
+
+				path.Reverse();
+
+				break;
+			}
+
+			//Check the neighbouring nodes for the best node to move to
+			foreach (Node neigh in GetNeighbour(currentNode)) 
+			{
+				if(!closedNodes.Contains(neigh))
+				{
+					int moveCost = currentNode.gCost + GetDistance(currentNode, neigh);
+
+					if(moveCost < neigh.gCost || !openNodes.Contains(neigh))
+					{
+						neigh.gCost = moveCost;
+						neigh.hCost = GetDistance(neigh, endNode);
+						neigh.previousNode = currentNode;
+
+						if(!openNodes.Contains(neigh))
+						{
+							openNodes.Add(neigh);
+						}
+					}
+				}
+			}
+		}
+
+		//Once path has been checked start to move
+		Move();
+	}
+
+	public void Move()
+	{
+		//If there is still a path move the game object to the next node
+		if(path.Count > 0)
+		{
+			transform.position = Vector3.Lerp(transform.position, path[0].position, 0.05f);
+		}
+	}
+
+	//Get all neighbouring nodes
+	public List<Node> GetNeighbour(Node node)
+	{
+		List<Node> neighbours = new List<Node>();
+
+		for (int i = -1; i <= 1; i++) 
+		{
+			for (int j = -1; j <= 1; j++) 
+			{
+				if(i == 0 && j == 0)
+				{
+					continue;
+				}
+					
+				int x = node.xposition + i;
+				int y = node.yposition + j;
+
+				if(x >= 0 && x < totalNodesX && y >= 0 && y < totalNodesY)
+				{
+					neighbours.Add(nodes[x,y]);
+				}
+			}
+		}
+
+		return neighbours;
+	}
+
+	//Calculate the distance from starting node to the next node
+	public int GetDistance(Node start, Node check)
+	{
+		int xDistance = Mathf.Abs(start.xposition - check.xposition);
+		int yDistance = Mathf.Abs(start.yposition - check.yposition);
+
+		if(xDistance > yDistance)
+		{
+			return 14 * yDistance + 10 * (xDistance - yDistance);
+		}
+		else
+		{
+			return 14 * xDistance + 10 * (yDistance - xDistance);
+		}
+	}
 }
